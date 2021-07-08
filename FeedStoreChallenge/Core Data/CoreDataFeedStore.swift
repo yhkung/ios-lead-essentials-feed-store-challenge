6817d966efe06a935fb3e759331f2b389a53e7c7
YHKung <bowser.kung@gmail.com>
2021-07-08 18:06:54 +0800

    Extract `perform` logic into a shared helper method

--- FeedStoreChallenge/Core Data/CoreDataFeedStore.swift
+++ FeedStoreChallenge/Core Data/CoreDataFeedStore.swift
@@ -29,8 +29,7 @@ public final class CoreDataFeedStore: FeedStore {
 	}
 
 	public func retrieve(completion: @escaping RetrievalCompletion) {
-		let context = self.context
-		context.perform {
+		perform { context in
 			do {
 				if let cache = try ManagedCache.find(in: context) {
 					let feed = cache.localFeed
@@ -45,8 +44,7 @@ public final class CoreDataFeedStore: FeedStore {
 	}
 
 	public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-		let context = self.context
-		context.perform {
+		perform { context in
 			do {
 				let managedCache = try ManagedCache.newUniqueInstance(in: context)
 				managedCache.timestamp = timestamp
@@ -62,8 +60,7 @@ public final class CoreDataFeedStore: FeedStore {
 	}
 
 	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-		let context = self.context
-		context.perform {
+		perform { context in
 			do {
 				try ManagedCache.find(in: context)
 					.map(context.delete)
@@ -74,5 +71,10 @@ public final class CoreDataFeedStore: FeedStore {
 				completion(error)
 			}
 		}
+	}
+
+	private func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
+		let context = self.context
+		context.perform { action(context) }
 	}
 }
